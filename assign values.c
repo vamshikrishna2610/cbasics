6d57f582959d153dbc1ed10eeee250379d3641d2
vamshikrishna2610 <93246864+vamshikrishna2610@users.noreply.github.com>
2021-11-17 12:25:57 +0530

    Add files via upload

--- assign values.c
+++ assign values.c
@@ -2,17 +2,17 @@
 void main()
 {
 	int a, b, c;
-	a = 10,
-	b = 4,
-	c = 3,
-	a += b; // 14, 4, 3
-    a -= c; // 11, 4, 3
-    c *= a; // 11, 4, 33
-    a /= b; // 2, 4, 33
-    b *= c; // 2, 132, 33
-    a += b; // 134, 132, 33
+	printf("INPUT:\n");
+	scanf("%d %d %d", &a, &b, &c);
+	printf("OUTPUT:\n");
+	printf("%d %d %d\n", a += b, b, c);
+	printf("%d %d %d\n", a -= c, b, c);
+	printf("%d %d %d\n", a, b, c *= a);
+	printf("%d %d %d\n", a /= b, b, c);
+	printf("%d %d %d\n", a, b *= c, c);
+	printf("%d %d %d\n", a += b, b, c);
 	
 	
-
-	printf("%d %d %d", a, b, c);
+	
+	
 }
