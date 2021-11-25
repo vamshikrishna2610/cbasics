008784ce4f26fd0c5e38b5c6dac9dfdecfb52ee2
vamshikrishna2610 <93246864+vamshikrishna2610@users.noreply.github.com>
2021-11-25 19:14:35 +0530

    Add files via upload

---
+++ sum of integers1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ void main()
+  { 
+    int a,b,sum,subract,multiply;
+    int a,b,fdivision;
+    printf("\nEnter a value");
+    scanf("%d",&a);
+    printf("\nEnter b value");
+    scanf("%d",&b);
+     sum=a+b;
+    printf("\nsum of %d and %d is %d ",a,b,sum);
+     subract=a-b;
+     printf("\nsubract of %d and %d  is %d",a,b,subract);
+     multiply=a*b;
+     printf("\nmultiply of %d and %d is %d",a,b,multiply);
+     division=a/b;
+     printf("\n division of %f and %f  is %f",a,b,division);}
